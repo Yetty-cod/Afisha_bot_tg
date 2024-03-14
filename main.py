50a41a45d05cee118043d60255a7dd9d93cade31
Yetty-cod <klisak.e@yandex.ru>
2024-03-14 14:21:50 +0300

    Попытка добавить команду для вывода расписания. Вылезает ошибка импорта парсера

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import logging
 from telegram.ext import Application, MessageHandler, filters, CommandHandler
+from parser import CinemaArtHollNorilskParser
 
 # Запускаем логгирование
 logging.basicConfig(
@@ -10,13 +11,13 @@ logger = logging.getLogger(__name__)
 
 
 async def afisha_today(update, context):
-    await update.message.reply_text('Hello!')
+    await update.message.reply_text(CinemaArtHollNorilskParser().get_events())
 
 
 def main():
     application = Application.builder().token('6764108898:AAFjiDWFaVNmxtnGhz6_AwcEl-zxPAHXDZU').build()
 
-    command_handler = CommandHandler("print_hello", afisha_today)
+    command_handler = CommandHandler("shedule", afisha_today)
     application.add_handler(command_handler)
 
     application.run_polling()
--- test.py
+++ test.py
@@ -1,10 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
-import datetime
+from parser import CinemaArtHollNorilskParser
 
 
 # response = requests.get('https://api.kinopoisk.dev/v1.4/movie/search?page=1&limit=10&query=Аватар',
 #                         headers={'X-API-KEY': 'VHS9Z8S-8ZJMVC0-M529YWV-WKRWP3R'})
 # print(response.json())
 
-a = datetime.date(2024, 3, 14)
+print(CinemaArtHollNorilskParser().get_events())
